5eea1012df4ce8ddcf4fe2daad7798c3b981c5f0
Jason Hinsperger <jason.hinsperger@sap.com>
2020-10-26 11:49:04 -0400

    Add files via upload

---
+++ exercises/HDLExercise_Athena.sql
@@ -0,0 +1,87 @@
+-- Create Remote Source 
+CREATE REMOTE SOURCE "ATHENASOURCE" ADAPTER "athena" CONFIGURATION '<?xml version="1.0" encoding="UTF-8"?><ConnectionProperties name="connectionproperties"><PropertyEntry name="adapterversion"></PropertyEntry><PropertyEntry name="connectionmode">Adapter Properties</PropertyEntry><PropertyEntry name="dsn"></PropertyEntry><PropertyEntry name="driver"></PropertyEntry><PropertyEntry name="workgroup">athena_rest_regression</PropertyEntry><PropertyEntry name="region">eu-central-1</PropertyEntry><PropertyEntry name="extraadapterproperties"></PropertyEntry></ConnectionProperties>'
+WITH CREDENTIAL TYPE 'PASSWORD' using 'user=AKIA3E3KHFBLD64DBVXC;password=Y2U8eLUaDItFag+6aHyu/NaRCwGrOHFH2orZhhOu';
+
+-- Add AWS certificate and S3 certificate 
+CREATE CERTIFICATE FROM '-----BEGIN CERTIFICATE-----
+MIIEDzCCAvegAwIBAgIBADANBgkqhkiG9w0BAQUFADBoMQswCQYDVQQGEwJVUzEl
+MCMGA1UEChMcU3RhcmZpZWxkIFRlY2hub2xvZ2llcywgSW5jLjEyMDAGA1UECxMp
+U3RhcmZpZWxkIENsYXNzIDIgQ2VydGlmaWNhdGlvbiBBdXRob3JpdHkwHhcNMDQw
+NjI5MTczOTE2WhcNMzQwNjI5MTczOTE2WjBoMQswCQYDVQQGEwJVUzElMCMGA1UE
+ChMcU3RhcmZpZWxkIFRlY2hub2xvZ2llcywgSW5jLjEyMDAGA1UECxMpU3RhcmZp
+ZWxkIENsYXNzIDIgQ2VydGlmaWNhdGlvbiBBdXRob3JpdHkwggEgMA0GCSqGSIb3
+DQEBAQUAA4IBDQAwggEIAoIBAQC3Msj+6XGmBIWtDBFk385N78gDGIc/oav7PKaf
+8MOh2tTYbitTkPskpD6E8J7oX+zlJ0T1KKY/e97gKvDIr1MvnsoFAZMej2YcOadN
++lq2cwQlZut3f+dZxkqZJRRU6ybH838Z1TBwj6+wRir/resp7defqgSHo9T5iaU0
+X9tDkYI22WY8sbi5gv2cOj4QyDvvBmVmepsZGD3/cVE8MC5fvj13c7JdBmzDI1aa
+K4UmkhynArPkPw2vCHmCuDY96pzTNbO8acr1zJ3o/WSNF4Azbl5KXZnJHoe0nRrA
+1W4TNSNe35tfPe/W93bC6j67eA0cQmdrBNj41tpvi/JEoAGrAgEDo4HFMIHCMB0G
+A1UdDgQWBBS/X7fRzt0fhvRbVazc1xDCDqmI5zCBkgYDVR0jBIGKMIGHgBS/X7fR
+zt0fhvRbVazc1xDCDqmI56FspGowaDELMAkGA1UEBhMCVVMxJTAjBgNVBAoTHFN0
+YXJmaWVsZCBUZWNobm9sb2dpZXMsIEluYy4xMjAwBgNVBAsTKVN0YXJmaWVsZCBD
+bGFzcyAyIENlcnRpZmljYXRpb24gQXV0aG9yaXR5ggEAMAwGA1UdEwQFMAMBAf8w
+DQYJKoZIhvcNAQEFBQADggEBAAWdP4id0ckaVaGsafPzWdqbAYcaT1epoXkJKtv3
+L7IezMdeatiDh6GX70k1PncGQVhiv45YuApnP+yz3SFmH8lU+nLMPUxA2IGvd56D
+eruix/U0F47ZEUD0/CwqTRV/p2JdLiXTAAsgGh1o+Re49L2L7ShZ3U0WixeDyLJl
+xy16paq8U4Zt3VekyvggQQto8PT7dL5WXXp59fkdheMtlb71cZBDzI0fmgAKhynp
+VSJYACPq4xJDKVtHCN2MQWplBqjlIapBtJUhlbl90TSrE9atvNziPTnNvT51cKEY
+WQPJIrSPnNVeKtelttQKbfi3QBFGmh95DmK/D5fs4C8fF5Q=-----END CERTIFICATE-----' COMMENT 'AWS';
+
+CREATE CERTIFICATE FROM '-----BEGIN CERTIFICATE-----
+MIIDdzCCAl+gAwIBAgIEAgAAuTANBgkqhkiG9w0BAQUFADBaMQswCQYDVQQGEwJJ
+RTESMBAGA1UEChMJQmFsdGltb3JlMRMwEQYDVQQLEwpDeWJlclRydXN0MSIwIAYD
+VQQDExlCYWx0aW1vcmUgQ3liZXJUcnVzdCBSb290MB4XDTAwMDUxMjE4NDYwMFoX
+DTI1MDUxMjIzNTkwMFowWjELMAkGA1UEBhMCSUUxEjAQBgNVBAoTCUJhbHRpbW9y
+ZTETMBEGA1UECxMKQ3liZXJUcnVzdDEiMCAGA1UEAxMZQmFsdGltb3JlIEN5YmVy
+VHJ1c3QgUm9vdDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAKMEuyKr
+mD1X6CZymrV51Cni4eiVgLGw41uOKymaZN+hXe2wCQVt2yguzmKiYv60iNoS6zjr
+IZ3AQSsBUnuId9Mcj8e6uYi1agnnc+gRQKfRzMpijS3ljwumUNKoUMMo6vWrJYeK
+mpYcqWe4PwzV9/lSEy/CG9VwcPCPwBLKBsua4dnKM3p31vjsufFoREJIE9LAwqSu
+XmD+tqYF/LTdB1kC1FkYmGP1pWPgkAx9XbIGevOF6uvUA65ehD5f/xXtabz5OTZy
+dc93Uk3zyZAsuT3lySNTPx8kmCFcB5kpvcY67Oduhjprl3RjM71oGDHweI12v/ye
+jl0qhqdNkNwnGjkCAwEAAaNFMEMwHQYDVR0OBBYEFOWdWTCCR1jMrPoIVDaGezq1
+BE3wMBIGA1UdEwEB/wQIMAYBAf8CAQMwDgYDVR0PAQH/BAQDAgEGMA0GCSqGSIb3
+DQEBBQUAA4IBAQCFDF2O5G9RaEIFoN27TyclhAO992T9Ldcw46QQF+vaKSm2eT92
+9hkTI7gQCvlYpNRhcL0EYWoSihfVCr3FvDB81ukMJY2GQE/szKN+OMY3EU/t3Wgx
+jkzSswF07r51XgdIGn9w/xZchMB5hbgF/X++ZRGjD8ACtPhSNzkE1akxehi/oCr0
+Epn3o0WC4zxe9Z2etciefC7IpJ5OCBRLbf1wbWsaY71k5h+3zvDyny67G7fyUIhz
+ksLi4xaNmjICq44Y3ekQEe5+NauQrz4wlHrQMz2nZQ/1/I6eYs9HRCwBXbsdtTLS
+R9I4LtD+gdwyah617jzV/OeBHRnDJELqYzmp
+-----END CERTIFICATE-----' COMMENT 'S3';
+
+-- Create PSE HTTPS and get CERTIFICATE_ID for AWS 
+CREATE PSE HTTPS;
+SELECT CERTIFICATE_ID FROM CERTIFICATES WHERE COMMENT = 'AWS';
+
+-- Alter PSE HTTPS to add certificate id 
+ALTER PSE HTTPS ADD CERTIFICATE 155693;
+
+-- Get CERTIFICATE_ID for S3
+SELECT CERTIFICATE_ID FROM CERTIFICATES WHERE COMMENT = 'S3';
+
+-- Alter PSE HTTPS to add certificate 
+ALTER PSE HTTPS ADD CERTIFICATE 155694;
+
+-- Set the purpose to REMOTE SOURCE 
+SET PSE HTTPS PURPOSE REMOTE SOURCE;
+
+
+--CREATE VIRTUAL TABLE ATHENA_REGION AT "ATHENASOURCE"."NULL"."tpch_sf1000"."region";
+
+
+-- Query 1
+SELECT sum("l_extendedprice" * "l_discount") AS revenue
+FROM HDLExercise.V_LINEITEM
+WHERE "l_shipdate" >= '1994-01-01'
+	AND "l_shipdate" < add_years( TO_DATE( '1994-01-01', 'YYYY-MM-DD' ), 1 )
+	AND --ASA date '1994-01-01' + interval '1' year
+	"l_discount" BETWEEN .06 - 0.01 AND .06 + 0.01
+	AND "l_quantity" < 24;
+-- Query 2
+
+Replicate table in HANA Cloud and note the difference in performance 
+select * from "ATH_nation";
+alter virtual table DBADMIN."ATH_nation" add shared snapshot replica;
+select * from "ATH_nation";
+
+
